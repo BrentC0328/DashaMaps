86819bc2410fcc2ddec5853b361ca9a3c43cbeae
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-03 16:33:55 -0400

    My linked list

--- src/main/java/MyLinkedList/MyLinkedList.java
+++ src/main/java/MyLinkedList/MyLinkedList.java
@@ -1,4 +1,23 @@
 package MyLinkedList;
 
 public class MyLinkedList {
+
+
+    private MyNode head = null;
+
+    public MyLinkedList(String key) {
+        head = new MyNode(key, null);
+
+    }
+
+    public MyNode getHead(){return head;}
+
+    public void add(String key, String value) {
+    }
+
+    private void addToEnd(MyNode currentNode, MyNode n){
+
+    }
+
+
 }
