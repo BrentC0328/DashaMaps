a7df8dccdb9ce792d41be2916d6eb74ad561c846
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-03 16:52:44 -0400

    size test written for linked list

--- src/main/java/MyLinkedList/MyLinkedList.java
+++ src/main/java/MyLinkedList/MyLinkedList.java
@@ -10,9 +10,29 @@ public class MyLinkedList {
 
     }
 
-    public MyNode getHead(){return head;}
+    public MyNode getHead(){return head;
+    }
 
-    public void add(String key, String value) {
+    public void add(String key, Integer value) {
+        MyNode currentNode = head;
+        while(currentNode.getNext() != null){
+            currentNode = currentNode.getNext();
+        }
+        currentNode.setNext(new MyNode(key, value));
+    }
+
+
+    public Integer size(){
+        MyNode currentNode = head;
+        Integer counter = 1;
+        while(currentNode.getNext() != null){
+            currentNode = currentNode.getNext();
+            counter++;
+        }
+        return counter;
+    }
+    public void delete(String key){
+
     }
 
     private void addToEnd(MyNode currentNode, MyNode n){
---
+++ src/test/java/MyLinkedList/MyLinkedListTest.java
@@ -0,0 +1,42 @@
+package MyLinkedList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyLinkedListTest {
+
+    @Test
+    public void getHead() {
+    }
+
+    @Test
+    public void addTest() {
+        MyLinkedList listy = new MyLinkedList("Manny");
+        String expectedKey = "Brent";
+        Integer expectedValue = 6;
+        MyNode expected = new MyNode("Brent", 6);
+
+        listy.add(expectedKey, expectedValue);
+        MyNode actual = listy.getHead().getNext();
+
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void sizeTest(){
+        MyLinkedList listy = new MyLinkedList("Manny");
+        String expectedKey = "Brent";
+        Integer expectedValue = 6;
+        MyNode expectedNode = new MyNode("Brent", 6);
+        Integer expected = 2;
+
+        listy.add(expectedKey, expectedValue);
+        Integer actual = listy.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+}
