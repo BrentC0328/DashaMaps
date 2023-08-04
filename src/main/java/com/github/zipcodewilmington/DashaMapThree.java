c97195e31fd5378d306b31c78651dd945e0adf9d
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-03 21:47:35 -0400

    DashaMapTwo

--- src/main/java/com/github/zipcodewilmington/DashaMapThree.java
+++ src/main/java/com/github/zipcodewilmington/DashaMapThree.java
@@ -1,4 +1,38 @@
 package com.github.zipcodewilmington;
 
+import MyLinkedList.MyNode;
+
 public class DashaMapThree extends DashaMap{
+    public DashaMapThree(){
+        super();
+    }
+
+    private String hashFunctionThree(String input){
+        if(input.length() > 1){
+            return String.valueOf(Character.toLowerCase(input.charAt(0)) +
+                    Character.toLowerCase(input.charAt(1)));
+        }
+        return null;
+    }
+
+    public void set(String key, Integer value){
+        String keyHash = hashFunctionThree(key);
+        int keyHashIndex = getBucketIndex(keyHash);
+        doorDash[keyHashIndex].add(key, value);
+    }
+
+    public Integer get(String key){
+        String keyHash = hashFunctionThree(key);
+        int keyHashIndex = getBucketIndex(keyHash);
+        MyNode result = doorDash[keyHashIndex].get(key);
+        return result.getData().getValue();
+    }
+
+    public Integer delete(String key){
+        String keyHash = hashFunctionThree(key);
+        int keyHashIndex = getBucketIndex(keyHash);
+        return doorDash[keyHashIndex].delete(key);
+    }
+
+
 }
